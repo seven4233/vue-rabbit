61d7527e06b20ef0810c5f64431bb6d0b290d47e
linfeng <1985858775@qq.com>
2023-05-10 14:36:53 +0800

    layout静态资源搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-
+import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
 
 </script>
 
 <template>
     <div>
-        首页
         <!-- 二级路由出口 -->
-        <router-view></router-view>
+        <LayoutNav/>
+        <LayoutHeader/>
+        <RouterView/>
+        <LayoutFooter/>
     </div>
 </template>
 
