8362a6e3f2ac8fe59358451ad823d01ae0cb8142
linfeng <1985858775@qq.com>
2023-05-10 15:17:00 +0800

    实现nav滚动吸顶

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,12 +2,14 @@
 import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
 import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
 import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+import LayoutFixed from '@/views/Layout/components/LayoutFixed.vue'
 
 </script>
 
 <template>
     <div>
         <!-- 二级路由出口 -->
+        <LayoutFixed/>
         <LayoutNav/>
         <LayoutHeader/>
         <RouterView/>
