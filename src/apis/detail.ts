519c07d580b5a4ac78381e9540ad5252a466772b
linfeng <1985858775@qq.com>
2023-05-11 23:48:32 +0800

    详情页-热榜-基础组件封装和渲染

---
+++ src/apis/detail.ts
@@ -0,0 +1,38 @@
+import httpInstance from '@/utils/http'
+
+/**
+ * 获取商品详情页面
+ * @param id
+ * @returns
+ */
+export function getDetailAPI<T>(id: string | string[]) {
+  return httpInstance.request<any, T>({
+    url: '/goods',
+    params: {
+      id,
+    },
+  })
+}
+
+/**
+ * 获取热榜商品
+ * @param {Number} id - 商品id
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜
+ * @param {Number} limit - 获取个数
+ */
+type HotGoods = {
+  id: string | string[]
+  type: number
+  limit?: number
+}
+export function getHotGoodsAPI<T>(hotObj: HotGoods) {
+  const { id, type, limit = 3 } = hotObj
+  return httpInstance.request<any, T>({
+    url: '/goods/hot',
+    params: {
+      id,
+      type,
+      limit,
+    },
+  })
+}
