976bfbe06f52e8ab95b7c2b8a0d3bbf5d362e9fb
linfeng <linfeng@qq.com>
2023-05-14 16:13:23 +0800

    接口购物车-加入购物车的实现

---
+++ src/apis/cart.ts
@@ -0,0 +1,30 @@
+import myRequest from '@/utils/http'
+
+interface InsertCart {
+  skuId: string
+  count: number
+}
+/**
+ * 添加购物车接口
+ */
+export function insertCartAPI<T>(insertCartObj: InsertCart) {
+  const { skuId, count } = insertCartObj
+  return myRequest.request<any, T>({
+    url: '/member/cart',
+    method: 'POST',
+    data: {
+      skuId,
+      count,
+    },
+  })
+}
+
+/**
+ * 获取最新购物车列表
+ */
+
+export function getNewCartListAPI<T>() {
+  return myRequest.request<any, T>({
+    url: '/member/cart',
+  })
+}
--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
-
+import { useUserStore } from './user'
+import { getNewCartListAPI, insertCartAPI } from '@/apis/cart'
 export interface SkuObj {
   skuId: string
   price: string
@@ -21,15 +22,26 @@ export interface CartObj extends SkuObj {
 export const useCartStore = defineStore(
   'cart',
   () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
+    // 定义state
     const cartList = ref<CartObj[]>([])
 
     // 添加购物车
-    const addCart = (cartObj: CartObj) => {
-      const tarObj = cartList.value.find(item => item.skuId === cartObj.skuId)
-      if (tarObj) {
-        tarObj.count++
+
+    const addCart = async (cartObj: CartObj) => {
+      if (isLogin.value) {
+        // 登录之后的购物车逻辑
+        await insertCartAPI({ skuId: cartObj.skuId, count: cartObj.count })
+        const res = await getNewCartListAPI<IReturnType<any>>()
+        cartList.value = res.result
       } else {
-        cartList.value.push(cartObj)
+        const tarObj = cartList.value.find(item => item.skuId === cartObj.skuId)
+        if (tarObj) {
+          tarObj.count++
+        } else {
+          cartList.value.push(cartObj)
+        }
       }
     }
 
@@ -52,10 +64,18 @@ export const useCartStore = defineStore(
     }
 
     // 计算属性
+    // 1. 总的数量
     const totalCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+    // 2. 总价
     const totalPrice = computed(() => cartList.value.reduce((a, c) => a + +c.price * c.count, 0))
+    // 3. 已选择的数量
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
+    // 4. 已选择的商品价格合计
+    const selectedPrice = computed(() =>
+      cartList.value.filter(item => item.selected).reduce((a, c) => a + +c.price * c.count, 0)
+    )
 
-    // 多选
+    // 是否全选
     const isAll = computed(() => cartList.value.every(item => item.selected))
     return {
       cartList,
@@ -66,6 +86,8 @@ export const useCartStore = defineStore(
       totalCount,
       totalPrice,
       isAll,
+      selectedCount,
+      selectedPrice,
     }
   },
   {
