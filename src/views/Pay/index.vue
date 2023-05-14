543bda13adae1135c483a50f67354d8c752b2ddd
linfeng <linfeng@qq.com>
2023-05-14 23:18:43 +0800

    支付-倒计时优化

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { getOrderAPI } from '@/apis/pay'
 import { useRoute } from 'vue-router'
-import { ref, onMounted } from 'vue'
-
+import { ref, onMounted, onUnmounted } from 'vue'
+import { useCountDown } from '@/composables/useCountDown'
+const { start, formatTime, timer } = useCountDown()
 const route = useRoute()
 const payInfo = ref<any>({})
 const getPayInfo = async () => {
   const res: any = await getOrderAPI(route.query.id)
   payInfo.value = res.result
+  start(res.result.countdown)
 }
 
 onMounted(() => getPayInfo())
-
+onUnmounted(() => {
+  timer && clearInterval(timer)
+})
 // 跳转支付
-
 // 支付地址
 const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
-const backURL = 'http://127.0.0.1:5173/paycallback'
+const backURL = 'http://localhost:5173/paycallback'
 const redirectUrl = encodeURIComponent(backURL)
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
@@ -29,7 +32,10 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>
+            支付还剩 <span>{{ formatTime }}</span
+            >, 超时后将取消订单
+          </p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
